92f98d47c60604649b1c16fe56e01e1530821bc6
LeMinhChien-06 <chienlmph36639@fpt.edu.vn>
2025-06-17 15:11:39 +0700

    feat: add api get book image with  MinIO

--- src/main/java/com/example/management/controller/FileController.java
+++ src/main/java/com/example/management/controller/FileController.java
@@ -1,6 +1,11 @@
 package com.example.management.controller;
 
 import com.example.management.config.QRCodeConfigProperties;
+import com.example.management.config.minio.MinioConfigProperties;
+import com.example.management.entity.Book;
+import com.example.management.exception.book.BookExceptions;
+import com.example.management.repository.BookRepository;
+import io.minio.GetObjectArgs;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -23,6 +29,8 @@ import java.nio.file.Paths;
 public class FileController {
 
     private final QRCodeConfigProperties qrCodeConfigProperties;
+    private final MinioConfigProperties minioConfigProperties;
+    private final BookRepository bookRepository;
 
     @GetMapping("/qr/{fileName}")
     @Operation(summary = "Lấy ảnh QR", description = "API để lấy ảnh QR từ hệ thống lưu trữ")
@@ -45,5 +53,52 @@ public class FileController {
             log.error("Error retrieving QR image {}: {}", fileName, e.getMessage());
             return ResponseEntity.internalServerError().build();
         }
+    }
+
+    @GetMapping("/image/{bookId}")
+    @Operation(summary = "Lấy ảnh bìa sách", description = "Lấy ở bìa sách từ MinIO")
+    public ResponseEntity<byte[]> getBookImage(@PathVariable Long bookId) {
+        Book book = bookRepository.findById(bookId).orElseThrow(BookExceptions::bookNotFound);
+
+        String imageUrl = book.getImageUrl();
+        if (imageUrl == null || imageUrl.isBlank()) {
+            log.warn("Book {} does not have an imageUrl", bookId);
+            return ResponseEntity.notFound().build();
+        }
+        try {
+            String objectName = extractObjectNameFromUrl(imageUrl);
+            log.info("Extracted object name: {}", objectName);
+
+            InputStream stream = minioConfigProperties.minioClient().getObject(
+                    GetObjectArgs.builder()
+                            .bucket(minioConfigProperties.getBucketName())
+                            .object(objectName)
+                            .build());
+
+            byte[] imageData = stream.readAllBytes();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(imageData);
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    public String extractObjectNameFromUrl(String imageUrl) {
+        String[] parts = imageUrl.split("/");
+        if (parts.length >= 3) {
+            StringBuilder objectName = new StringBuilder();
+            for (int i = 4; i < parts.length; i++) {
+                if (i > 4) objectName.append("/");
+                objectName.append(parts[i]);
+            }
+            return objectName.toString();
+        }
+        return null;
     }
 }
--- src/main/java/com/example/management/enums/Status.java
+++ src/main/java/com/example/management/enums/Status.java
@@ -2,4 +2,5 @@ package com.example.management.enums;
 
 public enum Status {
     BORROWED, RETURNED, OVERDUE
+    // MƯỢN, TRẢ LẠI, QUÁ HẠN
 }
